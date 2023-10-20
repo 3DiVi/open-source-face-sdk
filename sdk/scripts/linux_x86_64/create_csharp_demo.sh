635bfce8f84ab1eb2e6c15bbb943ce1fd6706b5d
Sergei Alabugin <120363570+sergei-alabugin@users.noreply.github.com>
2023-10-20 13:37:20 +0500

    Update create_csharp_demo.sh

--- sdk/scripts/linux_x86_64/create_csharp_demo.sh
+++ sdk/scripts/linux_x86_64/create_csharp_demo.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 DEMO_NAME=${1:-"face"}
+MAJOR=$(echo "$(dotnet --version)" | awk '{split($0, result, "."); print result[1]}')
+MINOR=$(echo "$(dotnet --version)" | awk '{split($0, result, "."); print result[2]}')
+UBUNTU_VERSION=$(echo "$(lsb_release -rs)" | awk '{split($0, result, "."); print result[1]}')
+
+if [[ ${UBUNTU_VERSION} < 20 ]];
+then
+    OPENCV_RUNTIME=OpenCvSharp4.runtime.ubuntu.${UBUNTU_VERSION}.04-x64
+else
+    OPENCV_RUNTIME=OpenCvSharp4_.runtime.ubuntu.20.04-x64
+fi
 
 mkdir -p build
 cd build
@@ -12,6 +22,10 @@ cp ../src/csharp_api/* ./csharp_${DEMO_NAME}_demo/
 
 cd ./csharp_${DEMO_NAME}_demo/
 find . -type f -exec sed -i "s@open_source_sdk.dll@libopen_source_sdk.so@g" {} +
+if [[ ${UBUNTU_VERSION} == 16 ]];
+then
+    find . -type f -exec sed -i "s@7.0@6.0@g" {} +
+fi
 cd ../
 
 dotnet sln csharp_${DEMO_NAME}_demo.sln add csharp_${DEMO_NAME}_demo/csharp_${DEMO_NAME}_demo.csproj
@@ -19,7 +33,7 @@ dotnet sln csharp_${DEMO_NAME}_demo.sln add csharp_${DEMO_NAME}_demo/csharp_${DE
 cd csharp_${DEMO_NAME}_demo
 
 dotnet add package OpenCvSharp4
-dotnet add package OpenCvSharp4_.runtime.ubuntu.20.04-x64 #Replace for correct Ubuntu version and CPU architecture. Find at https://www.nuget.org/packages
+dotnet add package ${OPENCV_RUNTIME}
 dotnet add package CommandLineParser
 
 dotnet publish --configuration Release --output bin/publish /p:AllowUnsafeBlocks=true
@@ -29,4 +43,3 @@ cp -r csharp_${DEMO_NAME}_demo/bin/publish/* ./make-install/bin
 cp ./make-install/lib/libopen_source_sdk.so ./make-install/bin/
 
 echo "Done!"
-
